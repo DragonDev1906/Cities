be6c9a023b7d2f01be8ae27a17f537fa0c0c6e43
Jens <jmw.1906@gmx.de>
2022-01-22 18:25:16 +0100

    Use smaller textures and changed planet radius

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,22 @@ function Planet() {
   const planetContainer = useRef<THREE.Group>(null!)
   const [map, topologyMap, roughnessMap] = useLoader(THREE.TextureLoader, [
     '/colour_1024x512.jpg', 
-    '/topology_4096x2048.jpg',
+    '/topology_1024x512.jpg',
     '/roughness_3600x1800.jpg'
   ])
-  // const mouse = {
-  //   x: 0,
-  //   y: 0
-  // }
-  // addEventListener('mousemove', (event) => {
-  //   mouse.x = (event.clientX / innerWidth) * 2 - 1
-  //   mouse.y = -(event.clientY / innerHeight) * 2 + 1
-  // })
   useFrame(() => {
     planet.current.rotation.y += 0.002
-    // planetContainer.current.rotation.x = -mouse.y * 0.25
-    // planetContainer.current.rotation.y = mouse.x * 0.25
   })
   return (
     <group ref={planet}>
       <mesh>
-        <sphereGeometry args={[1, 32*4, 32*4]} />
+        <sphereGeometry args={[10, 32*4, 32*4]} />
         <meshStandardMaterial
           map={map}
           bumpMap={topologyMap}
-          bumpScale={0.2}
+          bumpScale={3}
           displacementMap={topologyMap}
-          displacementScale={0.02}
+          displacementScale={0.2}
           roughnessMap={roughnessMap}
           />
       </mesh>
@@ -44,7 +34,7 @@ function Planet() {
 function App() {
   return (
     <div className="App">
-      <Canvas camera={{position: [0,0,3.333], fov: 45}}>
+      <Canvas camera={{position: [0,0,27], fov: 45}}>
         <Suspense fallback={null}>
           <ambientLight args={["0x888"]} />
           <directionalLight args={["#fff", 5]} position={[1,1,1]} />
