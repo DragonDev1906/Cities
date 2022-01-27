7d6d1dfdcf6e1d0da07d3bc51bc0f6c6aa80c7b4
Jens <jmw.1906@gmx.de>
2022-01-27 14:44:27 +0100

    Add responsive Header title+paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import Globe from './Globe'
 function App() {
   return (
     <div className="App">
-      <h1 className="App-Title">
-        Cities
-      </h1>
+      <div className="App-Header">
+        <h1>
+          CITIES
+        </h1>
+        <p>
+          Buy, hold and trade cities. Earn ERC-721 achievement tokens and climb the Leaderboard.
+        </p>
+      </div>
       <div className="App-GlobePadding">
         <div className="App-GlobeAspectRatioControl">
           <div>
