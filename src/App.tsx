3e2924383997cf23ebdf0a54dc0f51ca48b50c57
Jens <jmw.1906@gmx.de>
2022-01-31 19:56:14 +0100

    Setup web3 connector using web3-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
 import Header from './Header'
 import ActivityLog from './ActivityLog'
+import { getPriorityConnector, initializeConnector } from '@web3-react/core'
+import { MetaMask } from '@web3-react/metamask'
 
+const [metaMask, metaMaskHooks] = initializeConnector<MetaMask>((actions) => new MetaMask(actions))
+const { usePriorityConnector } = getPriorityConnector(
+  [metaMask, metaMaskHooks]
+)
 
 function App() {
+  const priorityConnector = usePriorityConnector()
+
   return (
     <div className="App">
       <Header />
