c5b1a38f13b93a9c5dfa5d6db70bda0e292a5cb9
Jens <jmw.1906@gmx.de>
2022-01-31 20:08:08 +0100

    Add navigation draft

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,13 +5,23 @@ import { Globe } from './components'
 function Header() {
   return (
     <div className="Header">
-      <div className="Header-Text">
-        <h1>
-          CITIES
-        </h1>
-        <p>
-          Buy, hold and trade cities. Earn ERC-721 achievement tokens and climb the Leaderboard.
-        </p>
+      <div className="Header-Textarea">
+        <div className="Header-Text">
+          <h1>
+            CITIES
+          </h1>
+          <p>
+            Buy, hold and trade cities. Earn ERC-721 achievement tokens and climb the Leaderboard.
+          </p>
+        </div>
+        <div className="Header-Navigation">
+          <nav>
+            <a>Home</a>
+            <a>Inventory</a>
+            <a>Leaderboard</a>
+          </nav>
+          <button>Connect Wallet</button>
+        </div>
       </div>
       <div className="Header-GlobePadding">
         <div className="Header-GlobeAspectRatioControl">
